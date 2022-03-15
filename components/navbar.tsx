6a123e39e0c41ea06660859114f8a6a3497c329f
naagaraa <ekabersinar@gmail.com>
2022-03-16 00:23:16 +0700

    :update img

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,25 +1,30 @@
 import { useRef } from "react";
+import Image from "next/image";
 
 export default function navbar() {
   return (
-    <div className='container py-5 px-3'>
+    <div className="container py-5 px-3">
       <nav className="navbar navbar-expand-lg navbar-light">
-         <div className="container-fluid">
-            <a className="navbar-brand">
-               <img src="/favicon-32x32.png" alt="LOGO" />
-            </a>
-            <div className="d-flex">
-               <ul className="navbar-nav">
-                  <li className="nav-item">
-                     <a className="nav-link active" aria-current="page" href="#">
-                        <h5>contact me</h5>
-                     </a>
-                  </li>
-               </ul>
-            </div>
-         </div>
+        <div className="container-fluid">
+          <a className="navbar-brand">
+            <Image
+              src="/favicon-32x32.png"
+              alt="Landscape picture"
+              width={32}
+              height={32}
+            />
+          </a>
+          <div className="d-flex">
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="#">
+                  <h5>contact me</h5>
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
       </nav>
-   </div>
+    </div>
   );
 }
-
