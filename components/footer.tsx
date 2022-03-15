db7069a55855e60e232d74609521eb78bc7246fe
naagaraa <ekabersinar@gmail.com>
2022-03-15 23:58:03 +0700

    :update sosmed

--- components/footer.tsx
+++ components/footer.tsx
@@ -7,12 +7,12 @@ export default function footer() {
       <div className="d-flex">
           <ul className="navbar-nav flex-row ">
             <li className="nav-item me-5">
-                <a className="nav-link active" aria-current="page" href="#">
+                <a className="nav-link active" aria-current="page" href="https://www.linkedin.com/in/nagara/">
                   <h2>Linkedln</h2>
                 </a>
             </li>
             <li className="nav-item me-5">
-                <a className="nav-link active" aria-current="page" href="#">
+                <a className="nav-link active" aria-current="page" href="https://www.youtube.com/channel/UCYsZhw6Mlk23Q-nUPP9t1YA">
                   <h2>Youtube</h2>
                 </a>
             </li>
